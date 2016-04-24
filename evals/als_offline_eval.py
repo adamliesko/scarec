09cfb98df36502c923332c76017acb4d511cc2a9
adamliesko <adamliesko@gmail.com>
2016-04-24 19:06:40 +0200

    Prepare als for running on VM

--- evals/als_offline_eval.py
+++ evals/als_offline_eval.py
@@ -19,67 +19,69 @@ import json
 
 #LOAD_TRAIN
 
-
-files = ['/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-01.log',
-        '/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-02.log',
-         '/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-03.log',
-         '/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-04.log',
-         '/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-05.log']
-for file in files:
-    with open(file) as f:
-        print(file)
-        x = 0
-        for line in f:
-            x += 1
-            print(file + ':' + str(x))
-            jsond = json.loads(line)
-
-            user_id_long = jsond['context']['simple'].get('57', None)
-            item_id_long = jsond['context']['simple'].get('25', None)
-            if user_id_long is None or str(user_id_long) == '0' or item_id_long is None:
-                continue
-            user_id = Utils.encode_attribute('user_id', user_id_long)
-            item_id = Utils.encode_attribute('item_id', item_id_long)
-            redis.sadd(train_users_key, user_id)
-            redis.sadd(train_users_items_key, ':'.join([str(user_id), str(item_id)]))
+#
+# files = ['/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-01.log',
+#         '/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-02.log',
+#          '/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-03.log',
+#          '/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-04.log',
+#          '/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-05.log']
+# for file in files:
+#     with open(file) as f:
+#         print(file)
+#         x = 0
+#         for line in f:
+#             x += 1
+#             print(file + ':' + str(x))
+#             jsond = json.loads(line)
+#
+#             user_id_long = jsond['context']['simple'].get('57', None)
+#             item_id_long = jsond['context']['simple'].get('25', None)
+#             if user_id_long is None or str(user_id_long) == '0' or item_id_long is None:
+#                 continue
+#             user_id = Utils.encode_attribute('user_id', user_id_long)
+#             item_id = Utils.encode_attribute('item_id', item_id_long)
+#             redis.sadd(train_users_key, user_id)
+#             redis.sadd(train_users_items_key, ':'.join([str(user_id), str(item_id)]))
 
 #
 # LOAD_TEST
 #
 
-files = ['/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-06.log',
-         '/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-07.log']
-for file in files:
-    with open(file) as f:
-        print(file)
-        x = 0
-        for line in f:
-            x += 1
-            print(x)
-            json = json.loads(line)
-            user_id_long = jsond['context']['simple'].get('57', None)
-            item_id_long = jsond['context']['simple'].get('25', None)
-            if user_id_long is None or str(user_id_long) == '0' or item_id_long is None:
-                continue
-            user_id = Utils.encode_attribute('user_id', user_id_long)
-            item_id = Utils.encode_attribute('item_id', item_id_long)
-            redis.sadd(test_users_key, user_id)
-            redis.sadd(test_users_items_key, ':'.join([str(user_id), str(item_id)]))
-            redis.sadd('als:eval:test:user_items:' + str(user_id), item_id)
+# files = ['/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-06.log',
+#          '/Users/Adam/PLISTA_DATASET/als_eval/impression_2013-06-07.log']
+# for file in files:
+#     with open(file) as f:
+#         print(file)
+#         x = 0
+#         for line in f:
+#             x += 1
+#             print(x)
+#             jsond = json.loads(line)
+#             user_id_long = jsond['context']['simple'].get('57', None)
+#             item_id_long = jsond['context']['simple'].get('25', None)
+#             if user_id_long is None or str(user_id_long) == '0' or item_id_long is None:
+#                 continue
+#             user_id = Utils.encode_attribute('user_id', user_id_long)
+#             item_id = Utils.encode_attribute('item_id', item_id_long)
+#             redis.sadd(test_users_key, user_id)
+#             redis.sadd(test_users_items_key, ':'.join([str(user_id), str(item_id)]))
+#             redis.sadd('als:eval:test:user_items:' + str(user_id), item_id)
 
 
 
 # FIGURE_OUT_USERS_TO_EVALUATE
-
+print('Getting user ids to eval')
 users_ids_to_evaluate = redis.sinter(train_users_key, test_users_key)
 
 # LOAD_DATA_INTO_THE_SPARK_RDD
+print('starting loading data')
 train_user_items_pre_rdd = redis.smembers(train_users_items_key)
 train_user_items_rdd = sc.parallelize(train_user_items_pre_rdd)
 train_user_items_rdd_split = train_user_items_rdd.map(lambda pair_string: pair_string.decode('utf-8').split(':'))
-train_RDD = train_user_items_rdd_split.map(lambda user, item: (user, item))
+train_RDD = train_user_items_rdd_split.map(lambda visit: (visit[0], visit[1], 1)) #use 1 for seen
 train_RDD.cache()
 
+print('finish data loading')
 # PREPARE_PARAMETERS_TO_TEST_OUT
 SEED = 42
 RANKS = [3, 5, 10, 15, 20, 25, 30, 40, 50]  # number of hidden latent factors
@@ -88,18 +90,20 @@ user_id_counts = len(users_ids_to_evaluate)
 
 # ITERATE OVER OPTIONS, BUILD MODEL ON TRAIN DATA, EVAL on TEST_DATA, STORE RESULTS
 for ranks in RANKS:
+    print(ranks)
     for iters in ITERATIONS:
+        print(iters)
         start = time.time()
         map_10 = 0
         map_20 = 0
         model = ALS.trainImplicit(train_RDD, ranks, seed=SEED,
-                                  iterations=iters, )
+                                  iterations=iters)
         delta = time.time() - start
         model_id = 'als:rank:' + str(ranks) + ':iterations:' + str(iters)
         redis.set('eval:als:time_taken:' + model_id, delta)
 
         for user_id in users_ids_to_evaluate:
-            recommendations = model.recommendProductsForUsers(user_id, 20)
+            recommendations = model.recommendProducts(user_id, 20)
             recs_10 = recommendations[:10]
             recs_20 = recommendations[10:]
             visited_items = redis.smembers('als:eval:test:user_items:' + str(user_id))
@@ -113,8 +117,8 @@ for ranks in RANKS:
             matched_20 = [r_id for r_id in recs_20_int if r_id in visited_items_int]
             matched_20_size = len(matched_20)
 
-            map_10 += matched_10_size/10
-            map_20 += matched_20_size/20
+            map_10 += matched_10_size/10.0
+            map_20 += matched_20_size/20.0
 
         map_10_final = map_10 / float(user_id_counts)
         map_20_final = map_20 / float(user_id_counts)
