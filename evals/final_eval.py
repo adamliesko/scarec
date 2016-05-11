ed452582f46f65d039edd9c8b95c32f333e2ab21
adamliesko <adamliesko@gmail.com>
2016-05-11 14:29:49 +0200

    xxx

--- evals/final_eval.py
+++ evals/final_eval.py
@@ -495,6 +495,8 @@ def global_eval_combined():
 
             ctx_recs = ctx_recs_glob[cluster]
             for r, v in ctx_recs:
+                print(r)
+                print(v)
                 if clustered_recs.get(r, None):
                     clustered_recs[r] = v * weight_of_cluster
                 else:
