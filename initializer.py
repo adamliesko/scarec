40884fdb9f09d29a94621a74d67f90222838d702
adamliesko <adamliesko@gmail.com>
2016-05-13 01:38:24 +0200

    combo uberkill

--- initializer.py
+++ initializer.py
@@ -1,11 +1,11 @@
 from clustering.clustering_model import ClusteringModel
 from classification.random_forester import RandomForester
-from collaborative.als import ALS
+from recommenders.collaborative_recommender import CollaborativeRecommender
 
 
 class MLInitializer:
     @staticmethod
     def init():
         ClusteringModel.load_model()
-        RandomForester.load_models()
-        ALS.load_model()
+        # RandomForester.load_models()
+        # CollaborativeRecommender.load_model()
